8d5a9ffd1df5a59b364e74b915bc1d10ff467550
Wahyu Pribadi <wahyupribadi17@gmail.com>
2014-11-26 17:36:11 +0700

    module project detail

--- project_detail.php
+++ project_detail.php
@@ -17,8 +17,6 @@
     <link href="css/plugins/morris.css" rel="stylesheet">
     <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <link href="css/plugins/dataTables.bootstrap.css" rel="stylesheet">
-
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 
@@ -35,63 +33,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Project</h1>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Detail Project
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="row">
-                                        <div class="col-lg-2">
-                                            <label>Project Name</label>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <p>Aplikasi Perpustakaan</p>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-2">
-                                            <label>Customer</label>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <p>PT. Sagara Xinix Solusitama</p>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-2">
-                                            <label>Start Project</label>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <p>1 - January - 2014</p>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-2">
-                                            <label>End Project</label>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <p>20 - May - 2014</p>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-2">
-                                            <label>Project Manager</label>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <p>Wahyu Pribadi</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <h1 class="page-header">Project / Aplikasi Perpustakaan</h1>
                 </div>
             </div>
 
@@ -121,7 +63,7 @@
                                 <div>
                                     <p>
                                         <strong>Deadline</strong>
-                                        <span class="pull-right text-muted">60% Complete</span>
+                                        <span class="pull-right text-muted">35 Days Again</span>
                                     </p>
                                     <div class="progress progress-striped active">
                                         <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
@@ -138,60 +80,142 @@
                 </div>
             </div>
 
-                        <div class="row">
+            <div class="row">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
-                        <div class="panel-heading">List Project</div>
-                        <!-- /.panel-heading -->
+                        <div class="panel-heading">
+                            Detail Project
+                        </div>
                         <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>Name</th>
-                                            <th>Customer</th>
-                                            <th>Start Project</th>
-                                            <th>End Project</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td><a href="project_detail.php">Aplikasi Perpustakaan</a></td>
-                                            <td>PT. Sagara Xinix Solusitama</td>
-                                            <td>1 - January - 2014</td>
-                                            <td>20 - Mey - 2014</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td><a href="project_detail.php">Aplikasi Perpustakaan</a></td>
-                                            <td>PT. Sagara Xinix Solusitama</td>
-                                            <td>1 - January - 2014</td>
-                                            <td>20 - Mey - 2014</td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td><a href="project_detail.php">Aplikasi Perpustakaan</a></td>
-                                            <td>PT. Sagara Xinix Solusitama</td>
-                                            <td>1 - January - 2014</td>
-                                            <td>20 - Mey - 2014</td>
-                                        </tr>
-                                        <tr>
-                                            <td>4</td>
-                                            <td><a href="project_detail.php">Aplikasi Perpustakaan</a></td>
-                                            <td>PT. Sagara Xinix Solusitama</td>
-                                            <td>1 - January - 2014</td>
-                                            <td>20 - Mey - 2014</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="row">
+                                        <div class="col-lg-2">
+                                            <label>Project Name</label>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <p>: Aplikasi Perpustakaan</p>
+                                        </div>
+                                        <div class="col-lg-2">
+                                            <a href="user_update.php" style="float: right;"><button class="btn btn-primary">Update</button></a>
+                                        </div>
+                                    </div>
 
+                                    <div class="row">
+                                        <div class="col-lg-2">
+                                            <label>Customer</label>
+                                        </div>
+                                        <div class="col-lg-10">
+                                            <p>: PT. Sagara Xinix Solusitama</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-lg-2">
+                                            <label>Start Project</label>
+                                        </div>
+                                        <div class="col-lg-10">
+                                            <p>: 1 - January - 2014</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-lg-2">
+                                            <label>End Project</label>
+                                        </div>
+                                        <div class="col-lg-10">
+                                            <p>: 20 - May - 2014</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Crew Project
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <table class="table table-striped table-bordered table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Wahyu Pribadi</td>
+                                                <td>Project Manager</td>
+                                                <td>Delete</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Adoel Razman</td>
+                                                <td>Programmer</td>
+                                                <td>Delete</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Wahyu Taufik</td>
+                                                <td>Programmer</td>
+                                                <td>Delete</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Sentot Bugraha</td>
+                                                <td>Programmer</td>
+                                                <td>Delete</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <form role="form" action="user_action.php" method="POST">
+                                                <div class="row">
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label>Name</label>
+                                                            <select name="religion" class="form-control">
+                                                                <option>...</option>
+                                                                <option value="">Option</option>
+                                                                <option value="">Option</option>
+                                                                <option value="">Option</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label>Position</label>
+                                                            <select name="religion" class="form-control">
+                                                                <option>...</option>
+                                                                <option value="">Option</option>
+                                                                <option value="">Option</option>
+                                                                <option value="">Option</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <button type="submit" class="btn btn-primary">Add</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
 
 
@@ -203,20 +227,6 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/plugins/metisMenu/metisMenu.min.js"></script>
     <script src="js/sb-admin-2.js"></script>
-
-    <!-- DataTables JavaScript -->
-    <script src="js/plugins/dataTables/jquery.dataTables.js"></script>
-    <script src="js/plugins/dataTables/dataTables.bootstrap.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="js/sb-admin-2.js"></script>
-
-    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
-    <script>
-    $(document).ready(function() {
-        $('#dataTables-example').dataTable();
-    });
-    </script>
 
 </body>
 
